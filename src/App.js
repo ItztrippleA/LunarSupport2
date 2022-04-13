e98531b39c0e48ce1a68b7be89b54a798e9de607
AdamsAFTJ <adams@aftjdigital.com>
2022-04-13 15:20:31 +0100

    Implement Navbar and changed theme color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 
-function App() {
+const Home = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Subscribe to </p>
+        <h1>Thapa Technical Home Page</h1>
+      </section>
+    </>
   );
-}
+};
+
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Lunar Support About Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Service = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Lunar Support Service Page</h1>
+      </section>
+    </>
+  );
+};
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <section className="hero-section">
+        <p>Welcome to </p>
+        <h1>Lunar Support Contact Page</h1>
+      </section>
+    </>
+  );
+};
+
+const App = () => {
+  const AppRoutes = () => {
+    const routes = useRoutes([
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/service",
+        element: <Service />,
+      },
+    ]);
+    return routes;
+  };
+  return (
+    <Router>
+      <AppRoutes />
+    </Router>
+  );
+};
 
 export default App;
